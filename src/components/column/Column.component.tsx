2b99bdb671df418bdc1463aa68d35b079e1dbca8
Mario Mustapic <mariomustapicvkci@gmail.com>
2022-11-05 14:10:34 +0100

    Added cloud/sun icons according to cloudcover

--- src/components/column/Column.component.tsx
+++ src/components/column/Column.component.tsx
@@ -1,8 +1,17 @@
 import "./Column.styles.scss";
+import clouds100 from "../../assets/images/icons8-cloud-80.png";
+import clouds50 from "../../assets/images/icons8-partly-cloudy-day.gif";
+import clouds0 from "../../assets/images/icons8-sun.gif";
 
 type Props = { [index: number | string]: number | string | any[] | any };
 
 export const Column = (props: Props): JSX.Element => {
+  let clouds: string = "";
+  let cloudCover = props.hourlyData?.clouds;
+  if (66 < cloudCover) clouds = clouds100;
+  if (33 <= cloudCover && cloudCover <= 66) clouds = clouds50;
+  if (cloudCover < 33) clouds = clouds0;
+
   // console.log(props?.hourlyData);
   // let hourlyData = [props?.hourlyData];
   // const rows = hourlyData?.map((rowData: any) => (
@@ -13,7 +22,9 @@ export const Column = (props: Props): JSX.Element => {
       <p>{props.hourlyData.hour}:00</p>
       <p>{props.hourlyData.temperature}&#8451;</p>
       <p>{props.hourlyData.precipitation} mm</p>
-      <p>{props.hourlyData.clouds}</p>
+      <p>
+        {props.hourlyData.clouds} <img src={clouds} alt="clouds" />
+      </p>
     </div>
   );
 };
