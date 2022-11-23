cd87e6a5d2aac279607a1752a331d3faca482324
Mario Mustapic <mariomustapicvkci@gmail.com>
2022-11-23 19:09:21 +0100

    Some changes to API
    In preparation for selectable weather API

--- src/components/location/Location.component.tsx
+++ src/components/location/Location.component.tsx
@@ -6,7 +6,9 @@ type Props = {
   currentTime: number | string | any[] | any;
   className?: string;
   setLocation: Function;
+  setSelectedAPI: Function;
   location: stateData;
+  selectedAPI: API;
 };
 interface stateData {
   name?: string;
@@ -14,7 +16,13 @@ interface stateData {
   latitude: number;
   longitude: number;
 }
-
+interface API {
+  URL: string;
+  title: string;
+}
+interface APIs {
+  [key: number]: API;
+}
 export const Location = (props: Props): JSX.Element => {
   const [state, setState] = useState<stateData>();
 
@@ -44,6 +52,24 @@ export const Location = (props: Props): JSX.Element => {
     }
     navigator.geolocation.getCurrentPosition(success, error, options);
   }, [state?.latitude, state?.longitude, state?.accuracy]);
+
+  const listOfAPIs: APIs = [
+    {
+      title: "open-meteo.com",
+      URL: `https://api.open-meteo.com/v1/forecast?latitude=${state?.latitude}&longitude=${state?.longitude}&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,apparent_temperature,precipitation,rain,showers,snowfall,snow_depth,freezinglevel_height,weathercode,pressure_msl,surface_pressure,cloudcover,cloudcover_low,cloudcover_mid,cloudcover_high,evapotranspiration,et0_fao_evapotranspiration,vapor_pressure_deficit,windspeed_10m,windspeed_80m,windspeed_120m,windspeed_180m,winddirection_10m,winddirection_80m,winddirection_120m,winddirection_180m,windgusts_10m,temperature_80m,temperature_120m,temperature_180m,soil_temperature_0cm,soil_temperature_6cm,soil_temperature_18cm,soil_temperature_54cm,soil_moisture_0_1cm,soil_moisture_1_3cm,soil_moisture_3_9cm,soil_moisture_9_27cm,soil_moisture_27_81cm,shortwave_radiation,direct_radiation,diffuse_radiation,direct_normal_irradiance,terrestrial_radiation,shortwave_radiation_instant,direct_radiation_instant,diffuse_radiation_instant,direct_normal_irradiance_instant,terrestrial_radiation_instant,temperature_1000hPa,temperature_975hPa,temperature_950hPa,temperature_925hPa,temperature_900hPa,dewpoint_1000hPa,dewpoint_975hPa,dewpoint_950hPa,dewpoint_925hPa,dewpoint_900hPa,relativehumidity_1000hPa,relativehumidity_975hPa,relativehumidity_950hPa,relativehumidity_925hPa,relativehumidity_900hPa,cloudcover_1000hPa,cloudcover_975hPa,cloudcover_950hPa,cloudcover_925hPa,cloudcover_900hPa,windspeed_1000hPa,windspeed_975hPa,windspeed_950hPa,windspeed_925hPa,windspeed_900hPa,winddirection_1000hPa,winddirection_975hPa,winddirection_950hPa,winddirection_925hPa,winddirection_900hPa,geopotential_height_1000hPa,geopotential_height_975hPa,geopotential_height_950hPa,geopotential_height_925hPa,geopotential_height_900hPa&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant,shortwave_radiation_sum,et0_fao_evapotranspiration&timezone=auto`,
+    },
+    {
+      title: "7timer",
+      URL: `http://www.7timer.info/bin/api.pl?lon=${state?.longitude}&lat=${state?.latitude}&product=civil&output=json
+    `,
+    },
+  ];
+  if (
+    props.selectedAPI.URL === "" &&
+    state?.latitude !== undefined &&
+    state?.longitude !== undefined
+  )
+    props.setSelectedAPI(listOfAPIs[0]);
 
   return (
     <div className={props.className}>
